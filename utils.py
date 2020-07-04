fa43c42bccdbb6659b1f612d2216084d0640571b
Dinh Anh <dinhanhx@gmail.com>
2020-07-04 10:03:31 +0700

    Add get_actors_characters()

--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
 import requests
 from iso639 import to_iso639_1
 
+def sqr(s):
+    """
+    Single quote remover
+    """
+    return s.replace("'", "")
+
 def get_series(i):
     """
     Get tv series then make sql statement to insert into table series
@@ -9,7 +15,7 @@ def get_series(i):
         r = requests.get(f'http://api.tvmaze.com/shows/{i}')
         data = r.json()
         return f"INSERT INTO series (title, lang, current_status, rating, link) " \
-                f"VALUES ('{data['name']}', '{to_iso639_1(data['language'])}', " \
+                f"VALUES ('{sqr(data['name'])}', '{to_iso639_1(data['language'])}', " \
                 f"'{data['status']}', {data['rating']['average']}, '{data['url']}');\n"
     except:
         pass
@@ -24,7 +30,7 @@ def get_episodes(i):
         sql = f""
         for data in list_data:
             sql = sql + f"INSERT INTO episodes (series_id, episode_name, episode_number, " \
-            f"season_number, duration, release_date, link) VALUES ({i}, '{data['name']}', " \
+            f"season_number, duration, release_date, link) VALUES ({i}, '{sqr(data['name'])}', " \
             f"{data['number']}, {data['season']}, {data['runtime']}, {data['airdate']},  "\
             f"'{data['url']}');\n"
 
@@ -53,9 +59,9 @@ def get_creators(i):
         r = requests.get(f'http://api.tvmaze.com/shows/{i}/crew')
         data = r.json()[0]['person']
         return f"INSERT INTO creators (first_name, last_name, nationality, gender, " \
-        f"link) VALUES ('{data['name'].split()[0]}', '{data['name'].split()[1]}', " \
+        f"link) VALUES ('{sqr(data['name'].split()[0])}', '{sqr(data['name'].split()[-1])}', " \
         f"'{get_code(data['country'])}', {to_iso_5218(data['gender'])}, " \
-        f"'{data['url']}');"
+        f"'{data['url']}');\n"
 
     except:
         pass
@@ -65,17 +71,49 @@ def get_series_creators(i):
     Make sql statement to insert into table series_creators
     """
     return f"INSERT INTO series_creators (series_id, creator_id) VALUES " \
-    f"({i}, {i})"
+    f"({i}, {i});\n"
+
+def get_actors_characters(i_start, ii_end):
+    """
+    Get tv series' actors and characters
+    then make sql statement to insert into table actors and characters
+    """
+    def get_code(obj):
+        if obj is not None: return obj['code']
+
+        return None
+
+    counter = 0
+    sql = f""
+    for i in range(i_start, ii_end):
+        try:
+            r = requests.get(f'http://api.tvmaze.com/shows/{i}/cast')
+            list_data = r.json()
+            for data in list_data:
+                counter = counter + 1
+                person = data['person']
+                sql = sql + f"INSERT INTO actors (first_name, last_name, nationality, " \
+                f"gender, link) VALUES ('{sqr(person['name'].split()[0])}', '{sqr(person['name'].split()[-1])}', " \
+                f"'{get_code(person['country'])}', {to_iso_5218(person['gender'])}, '{person['url']}');\n"
+
+                character = data['character']
+                sql = sql + f"INSERT INTO characters (character_name, actor_id, series_id, link) " \
+                f"VALUES ('{sqr(character['name'])}', {counter}, {i}, '{character['url']}');\n"
+
+
+        except:
+            pass
+
+
+    return sql
 
 if __name__ == '__main__':
-    # f = open('insert_into_tables.sql', 'w', encoding = 'utf-8')
-    # for i in range(1, 11):
-    #     f.write(get_series(i))
-    #     f.write(get_episodes(i))
-    #     f.write(get_creators(i))
-    #     f.write(get_series_creators(i))
-    #
-    # f.close()
-
+    f = open('insert_into_tables.sql', 'w', encoding = 'utf-8')
     for i in range(1, 11):
-        print(get_series_creators(i))
+        f.write(get_series(i))
+        f.write(get_episodes(i))
+        f.write(get_creators(i))
+        f.write(get_series_creators(i))
+
+    f.write(get_actors_characters(1, 11))
+    f.close()
