61a1c85f857f3b53cb90b39924f97f2f708b798a
DDHLong <48487369+DDHLong@users.noreply.github.com>
2020-07-06 21:46:14 +0700

    Update create_procedures.sql
    add 2 more procedure

--- create_procedures.sql
+++ create_procedures.sql
@@ -2,15 +2,13 @@ CREATE PROCEDURE get_serie_by_year(@year INT(4))
 AS
 SELECT title, series_id 
 FROM series JOIN episodes
-WHERE episode_number = 1 AND release_date = @year
-GO;
+WHERE episode_number = 1 AND release_date = @year;
 
 CREATE PROCEDURE get_top_rating_series
 AS
 SELECT title AS TopRatingSeries
 FROM series
-WHERE rating >= 8
-GO;
+WHERE rating >= 8;
 
 CREATE PROCEDURE get_series_by_genres(@genre char(50))
 AS 
@@ -19,3 +17,15 @@ FROM series JOIN series_genres
 ON series.id = series_genres.series_id
 JOIN genre ON series_genres.genre_id = genres.id
 WHERE genres.genre_name = @genre;
+
+CREATE PROCEDURE get_rating_sorted
+AS
+SELECT title AS SortRating
+FROM series
+ORDER BY rating DESC;
+
+CREATE PROCEDURE get_series_by_status(@status char(1)) --'In Development': 'D','Ended':'E','Running':'R'
+AS 
+SELECT title, series_id
+FROM series
+WHERE @status = current_status;
